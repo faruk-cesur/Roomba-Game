ddba5048ca067342f570f6cc5c95ee26f9d6c00c
Faruk Cesur <faruk-cesur@hotmail.com>
2021-10-28 12:23:46 +0300

    Added MidpolyGames Assets & Added Managers & Removed Unnecessary Scripts

---
+++ Assets/_Project/Scripts/GameManager.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using UnityEngine;
+
+public enum GameState
+{
+    PrepareGame,
+    MainGame,
+    LoseGame,
+    WinGame
+}
+
+public class GameManager : MonoBehaviour
+{
+    private static GameManager _instance;
+    public static GameManager Instance => _instance;
+
+    private GameState _currentGameState;
+
+    public GameState CurrentGameState
+    {
+        get { return _currentGameState; }
+        set
+        {
+            switch (value)
+            {
+                case GameState.PrepareGame:
+                    break;
+                case GameState.MainGame:
+                    break;
+                case GameState.LoseGame:
+                    break;
+                case GameState.WinGame:
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(value), value, null);
+            }
+
+            _currentGameState = value;
+        }
+    }
+
+    private void Awake()
+    {
+        if (_instance != null && _instance != this)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            _instance = this;
+        }
+
+        CurrentGameState = GameState.PrepareGame;
+    }
+
+    public void StartGame()
+    {
+        CurrentGameState = GameState.MainGame;
+        UIManager.Instance.MainGameUI();
+        print("started game");
+    }
+
+    public void RestartGame()
+    {
+        UIManager.Instance.RetryButton();
+    }
+
+    public void NextLevel()
+    {
+        UIManager.Instance.NextLevelButton();
+    }
+
+    public void LoseGame()
+    {
+        StartCoroutine(UIManager.Instance.DurationLoseGameUI());
+    }
+
+    public void WinGame()
+    {
+        PlayerPrefs.SetInt("TotalGold", UIManager.Instance.gold + PlayerPrefs.GetInt("TotalGold"));
+        UIManager.Instance.UpdateGoldInfo();
+        CurrentGameState = GameState.WinGame;
+        SoundManager.Instance.PlaySound(SoundManager.Instance.winGameSound, 1);
+        StartCoroutine(UIManager.Instance.DurationWinGameUI());
+    }
+}
---
+++ Assets/_Project/Scripts/PlayerController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+
+public class PlayerController : MonoBehaviour
+{
+    #region Fields
+
+    [SerializeField] private float _robotSpeed;
+
+    #endregion
+
+    private void Update()
+    {
+        switch (GameManager.Instance.CurrentGameState)
+        {
+            case GameState.PrepareGame:
+                break;
+            case GameState.MainGame:
+                break;
+            case GameState.LoseGame:
+                break;
+            case GameState.WinGame:
+                break;
+            default:
+                throw new ArgumentOutOfRangeException();
+        }
+    }
+
+    #region PlayerMovement
+
+    #endregion
+
+
+    #region Methods
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Garbage garbage = other.GetComponentInParent<Garbage>();
+        if (garbage)
+        {
+            UIManager.Instance.gold++;
+            //Instantiate(particleCollectable, playerModel.transform.position + new Vector3(0, 2, 0), Quaternion.identity);
+            SoundManager.Instance.PlaySound(SoundManager.Instance.collectGarbageSound, 0.4f);
+            Destroy(other.gameObject);
+        }
+    }
+
+    #endregion
+}
